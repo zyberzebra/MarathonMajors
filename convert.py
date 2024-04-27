59d94f79b7426caeccb7a2daf18accbee5031305
zyberzebra <zyberzebra@users.noreply.github.com>
2024-04-27 16:45:43 +0200

    Implement script to convert CSV data to Markdown tables

---
+++ convert.py
@@ -0,0 +1,36 @@
+import csv
+import pandas as pd
+from marathon_urls import url_dict  # Add this line
+from tabulate import tabulate
+
+
+def format_as_markdown_table(df):
+    df = df.drop(columns='Year')
+    df.insert(1, 'Race URL', df["Race"].map(url_dict))  # insert URLs
+    md_table = tabulate(df, headers='keys', showindex=False, tablefmt='pipe')
+    return md_table
+
+
+def convert_csv_to_md(csv_filename, md_filename):
+    with open(csv_filename, 'r') as csv_file:
+        csv_data = list(csv.reader(csv_file))
+    # Prepare separate data for 2024 and 2025
+    data_2024 = [x for x in csv_data if x[0] == '2024']
+    data_2025 = [x for x in csv_data if x[0] == '2025']
+    # Create pandas DataFrame
+    df_2024 = pd.DataFrame(data_2024, columns=csv_data[0])
+    df_2025 = pd.DataFrame(data_2025, columns=csv_data[0])
+    # Create markdown tables
+    md_table_2024 = format_as_markdown_table(df_2024)
+    md_table_2025 = format_as_markdown_table(df_2025)
+    # Write to md file
+    with open(md_filename, 'w') as md_file:
+        md_file.write('## Marathon Majors 2025\n')
+        md_file.write(md_table_2025)
+        md_file.write('\n\n')
+        md_file.write('## Marathon Majors 2024\n')
+        md_file.write(md_table_2024)
+
+
+# Call the function provide csv and markdown file
+convert_csv_to_md('majors.csv', 'README.MD')
---
+++ marathon_urls.py
@@ -0,0 +1,9 @@
+# Contents of marathon_urls.py
+url_dict = {
+    'Tokyo': 'https://www.marathon.tokyo/en/',
+    'Boston': 'https://www.baa.org/races/boston-marathon',
+    'London': 'https://www.tcslondonmarathon.com/',
+    'Berlin': 'https://www.bmw-berlin-marathon.com/en/',
+    'Chicago': 'https://www.chicagomarathon.com/',
+    'New York': 'https://www.nyrr.org/tcsnycmarathon',
+}
